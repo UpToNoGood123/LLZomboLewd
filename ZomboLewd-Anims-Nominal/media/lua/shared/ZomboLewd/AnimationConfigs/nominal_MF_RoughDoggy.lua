7c2878b6d78b870731d6f89514181416b8b8f718
UpToNoGood <154734969+UpToNoGood123@users.noreply.github.com>
2023-12-25 16:36:18 +0000

    Remove additional stages from doggy
    Originally used to test multiple stages
    Also add criteria

--- ZomboLewd-Anims-Nominal/media/lua/shared/ZomboLewd/AnimationConfigs/nominal_MF_RoughDoggy.lua
+++ ZomboLewd-Anims-Nominal/media/lua/shared/ZomboLewd/AnimationConfigs/nominal_MF_RoughDoggy.lua
@@ -3,37 +3,23 @@ require "ZomboLewd/ZomboLewdConfig" --- Always include this at the top to forcel
 table.insert(ZomboLewdAnimationData, {
 	prefix = "Nominal_",
 	id = "Rough_Doggy",
-	tags = {"Receive_Vaginal", "Give_Vaginal", "Intercourse", "MF", "Anal", "Doggy", "Rough", "Sex"},
+	tags = {"Intercourse", "MF", "Anal", "Doggy", "Rough", "Sex"},
 	actors = {
 		{
+			criteria = {"bottom"},
 			stages = {
 				{
 					perform = "nominal_MF_RoughDoggy_F",
-					duration = 1000
-				},
-				{
-					perform = "nominal_MF_LegUpMissionary_F",
-					duration = 900
-				},
-				{
-					perform = "nominal_MF_DefeatedMissionary_F",
 					duration = 1000
 				}
 			}
 		},
 		{
 			gender = "Male",
+			criteria = {"top"},
 			stages = {
 				{
 					perform = "nominal_MF_RoughDoggy_M",
-					duration = 1000
-				},
-				{
-					perform = "nominal_MF_LegUpMissionary_M",
-					duration = 900
-				},
-				{
-					perform = "nominal_MF_DefeatedMissionary_M",
 					duration = 1000
 				}
 			}
